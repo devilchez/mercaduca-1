bde8653c9a5ac7e1e109169a518a05f50c49101b
Alexis-Marti <00065821@uca.edu.sv>
2025-06-17 16:19:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 from modulos.login import login
 from modulos.ventas import mostrar_ventas
 from modulos.abastecimiento import mostrar_abastecimiento
+from modulos.registro_emprendedor import registrar_emprendedor
 
 st.set_page_config(page_title="MERCADUCA", layout="centered")
 
@@ -16,12 +17,14 @@ else:
     tipo = st.session_state["tipo_usuario"]
     
     st.sidebar.title("Menú")
-    opcion = st.sidebar.selectbox("Ir a:", ["Ventas", "Abastecimiento", "Cerrar sesión"])
+    opcion = st.sidebar.selectbox("Ir a:", ["Ventas", "Abastecimiento", "Registrar Emprendedor", "Cerrar sesión"])
 
     if opcion == "Ventas" and tipo == "Administrador":
         mostrar_ventas()
     elif opcion == "Abastecimiento" and tipo in ["Asistente", "Administrador"]:
         mostrar_abastecimiento()
+     elif opcion == "Registrar Emprendedor" and tipo in ["Asistente", "Administrador"]:
+        registrar_emprendedor()
     elif opcion == "Cerrar sesión":
         st.session_state.clear()
         st.rerun()
