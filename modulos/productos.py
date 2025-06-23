a7d28155968fd87cd1e5245f292f090c2afc107a
devilchez <vilchezruizdelia@gmail.com>
2025-06-23 16:03:07 -0600

    Update productos.py

--- modulos/productos.py
+++ modulos/productos.py
@@ -71,7 +71,7 @@ def eliminar_productos(ids_a_eliminar):
 
 def mostrar_productos():
     """Muestra la tabla de PRODUCTO para edición y eliminación."""
-    st.header("Productos registrados")
+    st.header("📋 Productos registrados")
 
     df = obtener_productos()
     if df.empty:
