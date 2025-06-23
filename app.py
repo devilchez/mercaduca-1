ec3aa81b43355a1ad540c6bf60dcf0f0738b7a63
devilchez <vilchezruizdelia@gmail.com>
2025-06-23 16:01:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ else:
         registrar_emprendimiento()
     elif opcion == "Gestionar Emprendimiento" and tipo in ["Administrador"]:
         mostrar_emprendimientos()
-    elif opcion == "Registrar ProductoS" and tipo in ["Administrador"]:
+    elif opcion == "Registrar Productos" and tipo in ["Administrador"]:
         registrar_producto()
     elif opcion == "Gestionar Productos" and tipo in ["Administrador"]:
         mostrar_productos()
